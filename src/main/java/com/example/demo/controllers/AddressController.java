0f70756fa6bb457084ea069f03e1094272eaf741
Akshat_Sharma-15 <akshatsharma152002@gmail.com>
2025-03-17 13:33:04 +0530

    handling rest api's using service layer without db

--- src/main/java/com/example/demo/controllers/AddressController.java
+++ src/main/java/com/example/demo/controllers/AddressController.java
@@ -3,11 +3,15 @@ package com.example.demo.controllers;
 import java.util.*;
 import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.interfaces.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressController {
+
+    IEmployeeService iEmployeeService;
     //UC1 --> REST API's handled using responseDTO(without interference of service layer)
 
     @GetMapping("/res/get/{id}")
@@ -33,5 +37,32 @@ public class AddressController {
     @DeleteMapping("/res/delete/{id}")
     public ResponseDTO delete1(@PathVariable Long id){
         return new ResponseDTO("API triggered at /res/delete/{id}", "Success");
+    }
+
+    //UC2 --> Handling REST API's using Service layer without storing in DB
+
+    @GetMapping("/res2/get/{id}")
+    public ResponseDTO get2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/get/{id}", "Success");
+    }
+
+    @PostMapping("/res2/create")
+    public ResponseDTO create2(@RequestBody EmployeeDTO user){
+        return iEmployeeService.response("API triggered at /res/create", "Success");
+    }
+
+    @GetMapping("/res2/getAll")
+    public ResponseDTO getAll2(){
+        return iEmployeeService.response("API triggered at /res/getAll", "Success");
+    }
+
+    @PutMapping("/res2/edit/{id}")
+    public ResponseDTO edit2(@RequestBody EmployeeDTO user, @PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/edit/{id}", "Success");
+    }
+
+    @DeleteMapping("/res2/delete/{id}")
+    public ResponseDTO delete2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/delete/{id}", "Success");
     }
 }
